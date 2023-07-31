275cdec026ee3fdc6c5a3b11f7fcca39ceaa2890
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-07-31 20:21:29 +0300

    fix some undefined issues

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, describe, it, expect } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
